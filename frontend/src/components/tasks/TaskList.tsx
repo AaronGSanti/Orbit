870ad0461a98edba31aa745654327850836aa1ee
AaronGSanti <159558444+AaronGSanti@users.noreply.github.com>
2026-03-07 19:41:42 +0100

    frontend_ionic(3)

--- frontend/src/components/tasks/TaskList.tsx
+++ frontend/src/components/tasks/TaskList.tsx
@@ -1,58 +1,61 @@
-import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonIcon } from "@ionic/react";
-import { useEffect, useState } from "react";
-import { deleteTask, getTasks } from "../../services/task";
+import {
+    IonBadge,
+    IonButton,
+    IonButtons,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonIcon,
+} from "@ionic/react";
+import { Task } from "../../pages/Tarea";
+import { formatLabel } from "../../utils/text";
+import { getPriorityColor } from "../../utils/prioritycolor";
 import { pencil, trash } from "ionicons/icons";
 
-function TaskList(){
+type TaskListProps = {
+    tasks?: Task[];
+    onDelete: (id: number) => void;
+    onEdit: (id: number) => void;
+};
 
-    const [tasks, setTasks] = useState<any[]>([]);
-
-    useEffect(() => {
-        const fetchTasks = async() =>{
-            try{
-                const data = await getTasks();
-                setTasks(data.data);
-            }catch(error){
-                console.log("Error", error);
-            }
-        }
-        fetchTasks();
-    }, []);
-
-    //Funcion para eliminar una tarea
-    const handleDelete = async (id:number) =>{
-        try{
-            await deleteTask(id);
-            setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
-        }catch(error){
-            console.log("Error" , error);
-        }
-    }
-    return(
+const TaskList: React.FC<TaskListProps> = ({ tasks = [], onDelete , onEdit}) => {
+    return (
         <>
-            {/**LISTADO DE TAREAS */}
-            {tasks.map((task) => (
-                <IonCard key={task.id}>
-                <IonCardHeader>
-                    <IonCardTitle>{task.titulo}</IonCardTitle>
-                </IonCardHeader>
-                <IonCardContent>
-                    <p>Estado: {task.estado}</p>
-                    <p>Prioridad: {task.prioridad}</p>
+        {tasks.map((task) => (
+            <IonCard key={task.id}>
+            <IonCardHeader>
+                <IonCardTitle>{task.titulo}</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+                <p><strong>Estado:</strong>{formatLabel(task.estado)}</p>
+                <p>
+                    <strong>Prioridad: </strong>
+                    <IonBadge color={getPriorityColor(task.prioridad)}>
+                        {formatLabel(task.prioridad)}
+                    </IonBadge>
+                </p>
+                <p><strong>Fecha limite:</strong>{new Date(task.fecha_limite).toLocaleDateString()}</p>
 
-                    <div style={{display: "flex", justifyContent: "flex-end"}}>
-                        <IonButton fill="clear">
-                            <IonIcon icon={pencil} />
-                        </IonButton>
-                        <IonButton fill="clear" onClick={() => handleDelete(task.id)}>
-                            <IonIcon icon={trash}/>
-                        </IonButton>
-                    </div>
-                </IonCardContent>
+                
+                <div style={{display: "flex" , justifyContent: "flex-end"}}>
+                    <IonButtons>
+                    {/**Boton EDITAR */}
+                    <IonButton onClick={() => onEdit(task.id)}>
+                        <IonIcon icon={pencil}/>
+                    </IonButton>
+
+                    {/**Boton ELIMINAR */}
+                    <IonButton onClick={() => onDelete(task.id)}>
+                        <IonIcon icon={trash}/>
+                    </IonButton>
+                </IonButtons>
+                </div>
+            </IonCardContent>
             </IonCard>
-            ))}
+        ))}
         </>
-    )
-}
+    );
+};
 
 export default TaskList;
